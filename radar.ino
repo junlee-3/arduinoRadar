9dffb0ba7fa9d034bfea715c0e092e206283a916
Jun Lee <82641992+junlee-3@users.noreply.github.com>
2022-11-04 11:53:15 +1100

    Radar will filter contacts
    Will scan and if there is a contact it will return a value. Setup some LED Ring stuff too.

--- radar.ino
+++ radar.ino
@@ -1,12 +1,22 @@
+#include <Adafruit_NeoPixel.h>
+//First Ultrasonic
 #define trigPin D1
 #define echoPin D2 
+//Second Ultrasonic
 #define trigPin2 D7
 #define echoPin2 D8
+//LED Strip
+#define pixelCount 24 //Number of lights
+#define pixelPin D1
 
 long duration;
 long duration2;
 float distance; //In centimeters
 float distance2;
+bool contact;
+int pos;
+
+Adafruit_NeoPixel ring(pixelCount, pixelPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
@@ -15,6 +25,9 @@ void setup() {
   //Second Ultrasonic Sensor
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
+
+  ring.begin();
+  ring.setBrightness(25);
 }
 
 void loop() {
@@ -38,7 +51,17 @@ void loop() {
   duration2 = pulseIn(echoPin2, HIGH);
   distance2 = duration2 * 0.0343 /2;
 
-  Serial.println("Sensor 1 = " + String(distance));
-  Serial.println("Sensor 2 = " + String(distance2));
-  delay(1000);
+  //Serial.println("Sensor 1 = " + String(distance));
+  //Serial.println("Sensor 2 = " + String(distance2));
+
+  if (distance <= 40) {
+    Serial.println("Contact!");
+    Serial.println(distance);
+    delay(500);
+  } else {
+    Serial.println("No Contact!");
+    Serial.println(distance);
+    delay(500);
+  }
+  
 }
